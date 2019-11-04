eb54adc794a9809908127da616fa3053222783b7
Nimsaja <jasmin.roeper@nexgo.de>
2019-11-04 17:06:34 +0100

    change Field

--- Game/Color.js
+++ Game/Color.js
@@ -14,11 +14,15 @@ class Cell {
         let text = 'background-color : rgba(' + this.red + ',' + this.green + ',' + this.blue + ',' + this.alfa + ')';
         document.querySelector("#"+this.color).setAttribute("style", text);
     }
+
+    getColorString() {
+        return 'rgba(' + this.red + ',' + this.green + ',' + this.blue + ',' + this.alfa + ')';
+    }
 }
 
 // create random Array
 let arr = [];
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 900; i++) {
     r1 = Math.floor(Math.random() * 256) - 1;
     r2 = Math.floor(Math.random() * 256) - 1;
     r3 = Math.floor(Math.random() * 256) - 1;
@@ -26,5 +30,58 @@ for (let i = 0; i < 16; i++) {
     arr.push(c);
 }
 
-arr.forEach((el) => el.toString());
+// arr.forEach((el) => el.toString());
+
+function addElement (c, i) { 
+    // create a new div element 
+    var newDiv = document.createElement("span"); 
+    newDiv.id = "Cell"+i;
+    newDiv.style.backgroundColor = c.getColorString();
+    // and give it some content 
+    var newContent = document.createTextNode("[]"); 
+    // add the text node to the newly created div
+    newDiv.appendChild(newContent);  
+  
+    // add the newly created element and its content into the DOM 
+    const currentDiv = document.querySelector("#Cell"+i);
+    // currentDiv.setAttribute("style", 'background-color : rgba(200, 200, 200, 1)');
+    document.body.insertBefore(newDiv, currentDiv); 
+}
+
+function createNewLine() {
+    let newDiv = document.createElement("div");
+    const currentDiv = document.querySelector("div1");
+    document.body.insertBefore(newDiv, currentDiv);
+}
+
+for (let i = 0; i < arr.length; i++) {
+    if (i%30 == 0) {
+        createNewLine();
+    }
+    addElement(arr[i], i);
+}
+
+// Field is created now add some magic
+function changeField() {
+    let newArray = [];
+    newArray.push(arr[0]);
+
+    for (let i = 1; i < arr.length - 1; i++) {
+        // get DOM Element
+        const currentDiv = document.querySelector("#Cell"+i);
+
+        let cellLeft = arr[i-1];
+        let cellRight = arr[i+1];
+        let newColorCell =  new Cell(cellLeft.red, cellRight.green, cellLeft.blue, 1);
+        let color = newColorCell.getColorString();
+
+        currentDiv.style.backgroundColor = color;
+
+        newArray.push(newColorCell);
+    }
+
+    newArray.push(arr[arr.length-1]);
+
+    arr = newArray;
+}
 
