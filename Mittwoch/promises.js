54745a2d50a0cbb1bbe7b88d5faf6cddb45bef8c
Nimsaja <jasmin.roeper@nexgo.de>
2019-11-06 12:08:44 +0100

    verschachteltes promise

--- Mittwoch/promises.js
+++ Mittwoch/promises.js
@@ -8,13 +8,29 @@ var promise1 = new Promise(function(resolve, reject) {
         }
     }, 1000);
   });
+
+var promise2 = new Promise(function(resolve, reject) {
+    const r = Math.random();
+    setTimeout(() => {
+        if (r < 0.5) {
+            resolve('Success2!');
+        } else {
+            // reject('Rejected2!');
+        }
+    }, 1000);
+  });
   
 promise1.then(function(value) {
    console.log("Sucess...");
    console.log(value);
     // expected output: "Success!"
+
+    return Promise.resolve(promise2);
 }, function(value) {
     console.log("Rejected....");
+    console.log(value);
+}). then(function(value) {
+    console.log("Success 2...");
     console.log(value);
 });
 
